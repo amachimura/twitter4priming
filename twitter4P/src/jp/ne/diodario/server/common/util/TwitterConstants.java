0fee7433dd8dc2a976922b31a76e535f961422dd
diodario <machimura.akira@gmail.com>
2015-02-03 23:53:47 +0900

    make accesstoken service.

--- twitter4P/src/jp/ne/diodario/server/common/util/TwitterConstants.java
+++ twitter4P/src/jp/ne/diodario/server/common/util/TwitterConstants.java
@@ -2,7 +2,15 @@ package jp.ne.diodario.server.common.util;
 
 public class TwitterConstants {
 	/**
-	 * twitterAPIのベースURL
+	 * twitterAPIで用いる基底URL
 	 */
 	public static final String API_BASEURL = "https://api.twitter.com/1.1/";
+	
+	/**
+	 * リクエストトークンを取得するエンドポイント
+	 */
+	public static final String REQUESTTOKEN_ENDPOINT = API_BASEURL+"oauth/request_token";
+	
+	public static final String CONSUMER_KEY="rmBDbRp7eeSFoHl1iUSWuCbGI";
+	public static final String CONSUMER_SECRET="PNtoOnnjpCCmtQEiEJevIo7LMvWcDwbpv6GrbauB0uS3h9V0qq";
 }
